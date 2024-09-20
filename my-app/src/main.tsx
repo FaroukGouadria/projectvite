cc2a2f148113d6e28e75cec485c3c4d01a6018e1
farouk gouadria <gouadriaafar@gmail.com>
2024-09-20 09:23:48 +0100

    feat:set up chakraProvider at the root of the application

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
 
 createRoot(document.getElementById('root')!).render(
+  // 2. Wrap ChakraProvider at the root of your app
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
